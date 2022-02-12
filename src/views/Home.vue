3ca0a4023ed2de19a58aab2e0ed2db000bc6056d
Mojahid BELAMAN <mojahidbelaman@gmail.com>
2022-02-12 21:55:31 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/company.png" />
+    <HelloWorld msg="Welcome to `Test Internship`" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
